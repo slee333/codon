5880d4e1ac158ee67fc6c65f0329c9888b830a77
Seung Wook Lee <seungwooklee94@gmail.com>
2019-10-24 14:20:31 +0900

    Updates

---
+++ precache-manifest.c2087a6f699910a36d60e740c7ed2c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b582f07259f3cf779aafa39ac8943141",
+    "url": "/codon/index.html"
+  },
+  {
+    "revision": "fd39a8713a9fc4b828f1",
+    "url": "/codon/static/css/main.89b2b5a3.chunk.css"
+  },
+  {
+    "revision": "cf5da1ed66820583cef6",
+    "url": "/codon/static/js/2.c064e691.chunk.js"
+  },
+  {
+    "revision": "fd39a8713a9fc4b828f1",
+    "url": "/codon/static/js/main.e8111045.chunk.js"
+  },
+  {
+    "revision": "8d55d93e9bdd52f81a1b",
+    "url": "/codon/static/js/runtime-main.bdfc4c42.js"
+  }
+]);
